41a25fedf871e5a6c22294eef5f822f6790a18c7
Isaac <isaaclubanko@gmail.com>
2018-05-11 14:23:38 -0400

    added herkodb data

--- config/connection.js
+++ config/connection.js
@@ -5,16 +5,18 @@ require("dotenv").config({EMPTY: ""});
 var mysql = require("mysql");
 var keys = require("./keys.js")
 //var connection = mysql.createConnection(keys);
+var connection;
 
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
-
-
+if(process.env.JAWSDB_URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: '',
+      database: 'burgers_db'
+  });
+};
 
 module.exports = connection;
